a5be7a516d3494e7036df764e98934fe4287f298
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2020-03-21 21:29:48 -0700

    Move Map into own class

--- src/scene/game-scene.ts
+++ src/scene/game-scene.ts
@@ -2,6 +2,7 @@ import { BaddieSpawner } from '../entity/baddie/baddie-spawner';
 import { Tower } from '../entity/tower/tower';
 import { SlowEffect } from '../entity/effect/slow-effect';
 import { LigntningChainEffect } from '../entity/effect/lightning-chain-effect';
+import { Map as Map } from '../map/map';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
@@ -18,8 +19,9 @@ export class GameScene extends Phaser.Scene {
     private baddies: Phaser.GameObjects.Group;
     private towers: Phaser.GameObjects.Group;
 
+    private map: Map;
+
     private towerTypeIndex: number = 1;
-    private layer;
 
     constructor() {
         super(sceneConfig);
@@ -34,29 +36,9 @@ export class GameScene extends Phaser.Scene {
     }
 
     public create() {
-        const level = [
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 5, 6, 7, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 13, 14, 15, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 21, 22, 23, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 5, 6, 6, 6, 7, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 13, 14, 14, 14, 15, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 21, 14, 14, 14, 23, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 21, 14, 23, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 22, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29,  ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 30, 30, 30, 29, 29, 29, 29, 29, 29, 29,  ],
-            [29, 29, 29, 29, 29, 29, 30, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29,  ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29,  ],
-            [29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29,  ],
-        ];
-        const map = this.make.tilemap({ data: level, tileWidth: 32, tileHeight: 32 })
-        const tiles = map.addTilesetImage('desert-tiles');
-        this.layer = map.createStaticLayer(0, tiles, 0, 0);
-        map.setTileIndexCallback([14], this.applySlowEffect, this);
-        map.setTileIndexCallback([30], this.applyLigntningChainEffect, this);
+        this.map = new Map(this);
+        this.map.setTileIndexCallback([14], this.applySlowEffect, this);
+        this.map.setTileIndexCallback([30], this.applyLigntningChainEffect, this);
 
         this.towers = this.add.group({
             classType: Tower,
@@ -67,19 +49,19 @@ export class GameScene extends Phaser.Scene {
 
         this.spawners.push(new BaddieSpawner(this, this.baddies));
 
-        this.setupTowerTypeChangeEvents();
-        this.input.on('pointerup', this.towerAdd, this);
+        this.setupTowerEvents();
     }
 
     public update(time, delta) {
-        this.physics.collide(this.baddies, this.layer);
+        this.physics.collide(this.baddies, this.map.layer);
         this.spawners.forEach(s => s.update(time, delta));
     }
 
-    private setupTowerTypeChangeEvents() {
+    private setupTowerEvents() {
         this.input.keyboard.on('keydown-ONE', () => this.setTowerType(1), this)
         this.input.keyboard.on('keydown-TWO', () => this.setTowerType(2), this)
         this.input.keyboard.on('keydown-THREE', () => this.setTowerType(3), this)
+        this.input.on('pointerup', this.towerAdd, this);
     }
 
     private setTowerType(towerType: number) {
